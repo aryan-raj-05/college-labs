73c69ce39ec265cf723ec3a95eee6f78347ae8dc
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2024-12-24 13:26:14 +0000

    fixed fgets

--- patternReplace.c
+++ patternReplace.c
@@ -1,46 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define MAX_LEN 128
+#define MAX 256
 
-int findAndReplace(char *str, char *pat, char *repl, char *ans);
+int stringMatch(char str[], char pat[], char repl[], char ans[]) {
+    int flag = 0, strIter = 0, ansIter = 0, patIter = 0, matchIndex = 0;
 
-int main() {
-    char str[MAX_LEN], pat[MAX_LEN], repl[MAX_LEN], ans[MAX_LEN];
-
-    printf("Enter String: ");
-    fgets(str, MAX_LEN, stdin);
-
-    printf("Enter pattern: ");
-    fgets(pat, MAX_LEN, stdin);
-
-    printf("Enter Replacement: ");
-    fgets(repl, MAX_LEN, stdin);
-
-    int patExist = findAndReplace(str, pat, repl, ans);
-    if (patExist) {
-        printf("%s\n", ans);
-    } else {
-        printf("Pattern not found\n");
-    }
-
-    return 0;
-}
-
-int findAndReplace(char *str, char *pat, char *repl, char *ans) {
-    int patExist = 0;
-
-    int strIter = 0, matchIndex = 0, ansIter = 0, patIter = 0;
     while (str[strIter] != '\0') {
         if (str[matchIndex] == pat[patIter]) {
             matchIndex++; patIter++;
-            if (pat[patIter] == '\0') { // patter exists
-                patExist = 1;
-                // copy the replace string to ans string and set ansIter
-                // to specific val
-                for (int k = 0; repl[k] != '\0'; k++) {
-                    ans[ansIter++] = repl[k];
-                }
+            if (pat[patIter] == '\0') {
+                flag = 1;
+                for (int i = 0; repl[i] != '\0'; i++)
+                    ans[ansIter++] = repl[i];
                 strIter = matchIndex;
                 patIter = 0;
             }
@@ -51,6 +22,36 @@ int findAndReplace(char *str, char *pat, char *repl, char *ans) {
         }
     }
     ans[ansIter] = '\0';
-    
-    return patExist;
+    return flag;
 }
+
+void removeNewline(char *s) {
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == '\n') {
+            s[i] = '\0';
+            return;
+        }
+    }
+}
+
+int main() {
+    char str[MAX], pat[MAX], repl[MAX], ans[MAX];
+
+    printf("Enter String: ");
+    fgets(str, MAX, stdin);
+    removeNewline(str);
+
+    printf("Enter pattern to find: ");
+    fgets(pat, MAX, stdin);
+    removeNewline(pat);
+
+    printf("Enter Replacement: ");
+    fgets(repl, MAX, stdin);
+    removeNewline(repl);
+
+    int flag = stringMatch(str, pat, repl, ans);
+    if (flag)
+        printf("Modified String is: %s\n", ans);
+    else
+        printf("Pattern Doesn't exist\n");
+}   
